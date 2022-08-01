595c52d7dea2ecdce0195fe8dd092a01659083a4
Oluwatobi Okunuga <tobi@Oluwatobis-MacBook-Air.local>
2022-08-01 15:43:47 -0400

    created login component and router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import React from 'react';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
